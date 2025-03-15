4dd797c5f11cc385a5225eea2cfe927d684d8d6b
hasan <ehteshamullhasan@gmail.com>
2025-03-15 15:07:51 +0530

    first commit

---
+++ src/main/java/com/git/TestPsaGitApplication.java
@@ -0,0 +1,14 @@
+package com.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPsaGitApplication {
+
+	int x = 100;
+	public static void main(String[] args) {
+		SpringApplication.run(TestPsaGitApplication.class, args);
+	}
+
+}
