f499d55ef4a085b551afe7c5045959dd8a5f6783
EhteshamullHasan <120940754+EhteshamullHasan@users.noreply.github.com>
2025-03-15 15:30:09 +0530

    Update A.java
    added variables

--- src/main/java/com/git/A.java
+++ src/main/java/com/git/A.java
@@ -2,7 +2,9 @@ package com.git;
 
 public class A {
     public static void main(String[] args) {
-        int x = 10;int y = 20;
+        int x = 10;
+        int y = 20;
+        System.out.print(x+y);
     }
 
 }
